6dd53b5d4e8e1e6bffda0b27b4c3e5c74ac27f48
wust <1726932926@qq.com>
2022-03-09 11:09:57 +0800

    st新增企业home

---
+++ lib/page/module_enterprise/enterprise_home.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+class EnterpriseHome extends StatefulWidget {
+  const EnterpriseHome({Key? key}) : super(key: key);
+
+  @override
+  _EnterpriseHomeState createState() => _EnterpriseHomeState();
+}
+
+class _EnterpriseHomeState extends State<EnterpriseHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
---
+++ lib/page/module_protection_agency/protection_agency_home.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class ProtectionAgencyHome extends StatefulWidget {
+  const ProtectionAgencyHome({Key? key}) : super(key: key);
+
+  @override
+  _ProtectionAgencyHomeState createState() => _ProtectionAgencyHomeState();
+}
+
+class _ProtectionAgencyHomeState extends State<ProtectionAgencyHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
